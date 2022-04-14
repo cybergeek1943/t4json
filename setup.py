7a9ae40d7ba9b8d80f6931ce6ac6136151cf685a
cybergeek1943 <cybergeek.1943@gmail.com>
2022-04-14 16:44:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 setup(
-  name='t4json',         # How you named your package folder (MyLib)
-  packages=['t4json'],   # Chose the same as "name"
-  version='0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description='TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author='Isaac Wolford',                   # Type in your name
-  author_email='cybergeek.1943@gmail.com',      # Type in your E-Mail
-  url='https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url='https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords=['json', 'tool', 'tools', 'data', 'structures', 'flattening', 't4json'],   # Keywords that define your package best
+  name='t4json',
+  packages=['t4json'],
+  version='v1.0.0',
+  license='MIT',
+  description='Tools to work with JSON data easily and quickly.',
+  author='Isaac Wolford',
+  author_email='cybergeek.1943@gmail.com',
+  url='https://github.com/cybergeek1943/t4json',
+  download_url='https://github.com/cybergeek1943/t4json/archive/refs/tags/v1.0.0.tar.gz',
+  keywords=['json', 'tool', 'tools', 'data', 'structures', 'flattening', 't4json', 'flatten', 'nested'],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 5 - Production/Stable',  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License'
   ],
 )
