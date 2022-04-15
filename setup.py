b09059c5e9f6947ec17659c121cd58e6e8ffe569
cybergeek1943 <cybergeek.1943@gmail.com>
2022-04-14 21:43:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as rm:
 setup(
     name='t4json',
     packages=['t4json'],
-    version='v1.0.2',
+    version='v1.0.3',
     license='MIT',
     description='Tools to work with JSON data easily and quickly.',
     long_description=long_description,
@@ -14,13 +14,13 @@ setup(
     author='Isaac Wolford',
     author_email='cybergeek.1943@gmail.com',
     url='https://github.com/cybergeek1943/t4json',
-    download_url='https://github.com/cybergeek1943/t4json/archive/refs/tags/v1.0.2.tar.gz',
+    download_url='https://github.com/cybergeek1943/t4json/archive/refs/tags/v1.0.3.tar.gz',
     keywords=['json', 'tool', 'tools', 'data', 'structures', 'flattening', 't4json', 'flatten', 'nested'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.6'
     ],
 )
